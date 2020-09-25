7b2663dc452536cd7bbb1e0817b7a6e58e338122
xephyz <xephyzone@gmail.com>
2020-09-25 11:48:58 +0200

    Initial property-based testing
    Checked:
    - `prune` produces a legal polynomial
    - `add` is associative
    - `add` is commutative
    - `zero` is the additive identity
    - `-p` is the additive inverse of `p`
    - `mul` is associative

--- test/Tests.fs
+++ test/Tests.fs
@@ -7,6 +7,39 @@ open Poly
 open FsCheck
 open FsCheck.Xunit
 
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let one: Poly = [1]
+let zero: Poly = []
+
+
+[<Property>]
+let ``Legal property`` p = isLegal (prune p)
+
+
+let associative op (p1: Poly) (p2: Poly) (p3: Poly) =
+    op (op p1 p2) p3 = op p1 (op p2 p3)
+
+let commutative op (p1: Poly) (p2: Poly) =
+    op p1 p2 = op p2 p1
+
+let identity op (idenP: Poly) (p: Poly) =
+    let p = prune p
+    op p idenP = p && commutative op idenP p
+
+let invAdditive (p: Poly) =
+    add p (mulC -1 p) = zero
+
+
+[<Property>]
+let ``+ (Add) is associative`` () = associative add
+
+[<Property>]
+let ``+ (Add) is commutative`` () = commutative add
+
+[<Property>]
+let ``zero ([]) is the additive identity`` () = identity add zero
+
+[<Property>]
+let ``-p is the additive inverse of p`` () = invAdditive
+
+[<Property>]
+let ``* (mul) is associative`` () = associative mul
